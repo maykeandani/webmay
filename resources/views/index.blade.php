e462363a3fc0b9e1e8d4701def9571d14b14fa2b
mayke andani <maykeandani5@gmail.com>
2021-07-16 22:56:31 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Biodataku</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#!">Biodata Diri</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header - set the background image for the header in the line below-->
+        <header class="py-5 bg-image-full" style="background-image: url('https://source.unsplash.com/wfh8dDlNFOk/1600x900')">
+            <div class="text-center my-5">
+                <h1 class="text-white fs-3 fw-bolder">Mayke Andani Rohmaniar</h1>
+                <p class="text-white-50 mb-0">...</p>
+            </div>
+        </header>
+        <!-- Content section-->
+        <section class="py-5">
+            <div class="container my-5">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6">
+                        <h2>Biodata</h2>
+                        <p class="lead">Nama saya Mayke Andani Rohmaniar biasa dipaggil mayke atau keke saya lahir di Magelang tanggal 9 Mei 2002. Saya menyukai warna hitam putih dan biru. Saya anak ke-4 dari 5 bersaudara. Saya memiliki golongan darah A. Sekarang saya menempuh pendidikan di D4-Teknik Informatika di Poltekpos. Makanan kesukaan saya adalah semua makanan berbau pedas. Tapi sejak dikabarkan saya terkena maag akut saya sedikit mengurangi tingkat kepedasan makanan saya, karena jika saya melanggarnya saya pasti akan merasakan kesakitan. Kurang lengkap jika ada makan favorit tanpa minuman favorit bukan? Minuman favorit saya adalah susu kotak dan air mineral. Selama 12 tahun sekolah saya mengikuti beberapa perlombaan contohnya seperti lomba baris berbaris dan lomba karya tulis walaupun setiap perlmbaan saya tidak menang tetapi pengalaman dari setiap perlombaan lah yang saya cari. Dulu saya memiliki cita-cita menjadi polwan tetapi karena keadaan mata saya yang kurang memungkinkan membuat saya kehilangan harapan saya menjadi polwan. Tapi inilah jalan terbaik yang tuhan berikan kepada saya.</p>
+                        <p class="mb-0">Saya memiliki hobby memasak, menyanyi, menari, dan membaca novel</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Image element - set the background image for the header in the line below-->
+        <div class="py-5 bg-image-full" style="background-image: url('https://source.unsplash.com/4ulffa6qoKA/1200x800')">
+            <!-- Put anything you want here! The spacer below with inline CSS is just for demo purposes!-->
+            <div style="height: 20rem"></div>
+        </div>
+        <!-- Content section-->
+        <section class="py-5">
+            <div class="container my-5">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6">
+                        <h2>Riwayat Pendidikan dan Organisasi</h2>
+                        <p class="lead">Saya mulai menempuh pendidikan dari tingkat TK saya bersekolah di TK ABA Aisiyah. Dan dilanjutkan di SDN Kalinegoro 3. Lalu saya meneruskan di Mts N Kota Magelang saat menduduki tingkat smp ini saya mengikuti organisasi pramuka dan saya juga mengikuti ekstrakulikuler seperti drumband. Pada saat menempuh bangku sma saya memutuskan untuk melanjutkan di MAN Kota Magelang pada jenjang ini saya mengikuti organisasi PKS dan MPK tak hanya itu saya juga mengikuti ekstakulikuler Tilawah dan English club</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; biodata 2021</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
